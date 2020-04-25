c44216e8204ff8b0a00f30f6a61a5e22ffd4c10c
TaniaAlvarezDiaz <tani.alvarez.diaz@gmail.com>
2020-04-25 20:04:06 +0200

    completado apartado del servidor y añadido carrusel de imagenes

---
+++ scripts/webrtc.js
@@ -0,0 +1,88 @@
+window.onload = function() {
+    //Variables
+    const IMAGES = [
+        "./images/webrtc_server_case_login.JPG",
+        "./images/webrtc_server_case_offer.JPG",
+        "./images/webrtc_server_case_answer.JPG",
+        "./images/webrtc_server_case_candidate.JPG",
+        "./images/webrtc_server_case_leave.JPG"
+    ]
+
+    const ALTS = [
+        "Evento 'message', mensaje 'login'",
+        "Evento 'message', mensaje 'offer'",
+        "Evento 'message', mensaje 'answer'",
+        "Evento 'message', mensaje 'candidate'",
+        "Evento 'message', mensaje 'leave'"
+    ]
+
+    const FIGCAPTIONS = [
+        "Evento <b>message</b> - Mensaje <b>login</b>",
+        "Evento <b>message</b> - Mensaje <b>offer</b>",
+        "Evento <b>message</b> - Mensaje <b>answer</b>",
+        "Evento <b>message</b> - Mensaje <b>candidate</b>",
+        "Evento <b>message</b> - Mensaje <b>leave</b>",
+    ]
+
+    let currentPosition = 0;
+    let previousButton = document.querySelector('#previousButton');
+    let nextButton = document.querySelector('#nextButton');
+    let image = document.querySelector('#image');
+    let figcaption = this.document.querySelector('#figcaptionCarrusel');
+
+    //Events
+    nextButton.addEventListener('click', nextImage);
+    previousButton.addEventListener('click', backImage);
+
+    //Start
+    previousButton.style.visibility = 'hidden';
+    renderImage();
+    
+    //Functions
+    /**
+     * Function that changes the photo in the next position
+     */
+    function nextImage() {
+        currentPosition++;
+
+        //Do visible previusButton
+        if (previousButton.style.visibility == 'hidden') {
+            previousButton.style.visibility = 'visible'
+        }
+
+        //If las position, disable nextButton
+        if (currentPosition >= IMAGES.length - 1) {
+            nextButton.style.visibility = 'hidden';
+        }
+
+        renderImage();
+    }
+
+    /**
+     * Function that changes the photo in the previous position
+     */
+    function backImage() {
+        currentPosition--;
+
+        //Do visible nextButton
+        if (nextButton.style.visibility == 'hidden') {
+            nextButton.style.visibility = 'visible'
+        }
+
+        //If first position, disable previousButton
+        if(currentPosition <= 0)  {
+            previousButton.style.visibility = 'hidden'
+        }
+
+        renderImage();
+    }
+
+    /**
+     * Function that updates the image image depending on current position
+     */
+    function renderImage() {
+        image.style.backgroundImage = `url(${IMAGES[currentPosition]})`;
+        image.alt = ALTS[currentPosition];
+        figcaption.innerHTML = FIGCAPTIONS[currentPosition];
+    }
+}
